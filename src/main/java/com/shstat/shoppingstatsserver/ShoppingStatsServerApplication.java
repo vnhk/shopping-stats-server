6e1143ccf8e0df368c0dd0e5772697ac513aabf1
berluk12@gmail.com <berluk12@gmail.com>
2023-06-12 16:59:14 +0200

    Init

---
+++ src/main/java/com/shstat/shoppingstatsserver/ShoppingStatsServerApplication.java
@@ -0,0 +1,13 @@
+package com.shstat.shoppingstatsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingStatsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingStatsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shstat/shoppingstatsserver/ShoppingStatsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shstat.shoppingstatsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingStatsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
