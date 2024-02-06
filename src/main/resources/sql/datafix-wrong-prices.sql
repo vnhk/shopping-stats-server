12915ce07a8b8ab9e1ece59ab824e2c0fb24d118
berluk12@gmail.com <berluk12@gmail.com>
2024-02-06 10:41:31 +0100

    Wrong prices fix

--- src/main/resources/sql/datafix-wrong-prices.sql
+++ src/main/resources/sql/datafix-wrong-prices.sql
@@ -1,6 +1,20 @@
-SELECT * FROM scrapdb.product_based_on_date_attributes pda WHERE pda.price >= 90000; #verify first
-DELETE FROM scrapdb.product_based_on_date_attributes WHERE price >= 90000;
-DELETE FROM product_stats WHERE 1=1
+SELECT *
+FROM product_based_on_date_attributes pda
+WHERE pda.price >= 90000; #verify first
+DELETE
+FROM product_based_on_date_attributes
+WHERE price >= 90000;
+
+#can take hours:
+# SELECT pda.product_id, pda.id, pda.price, pda.scrap_date
+DELETE FROM product_based_on_date_attributes pda
+WHERE price >= 5 * (SELECT AVG(pda1.price)
+                   FROM product_based_on_date_attributes pda1
+                   WHERE pda1.product_id = pda.product_id
+                     AND pda1.price > 0);
+DELETE
+FROM product_stats
+WHERE 1 = 1
 
 
 
