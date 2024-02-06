21527883717c954941bd3ce14059bf9778293cc0
berluk12@gmail.com <berluk12@gmail.com>
2024-02-06 11:16:24 +0100

    Wrong prices fix

--- src/main/resources/sql/datafix-wrong-prices.sql
+++ src/main/resources/sql/datafix-wrong-prices.sql
@@ -15,6 +15,15 @@ WHERE id in (SELECT pda.id
                                      FROM product_based_on_date_attributes pda1
                                      WHERE pda1.product_id = pda.product_id
                                        AND pda1.price > 0));
+#faster but works only for actual_products:
+DELETE
+FROM product_based_on_date_attributes
+WHERE id in (SELECT pda.id
+             FROM product_based_on_date_attributes pda
+             WHERE pda.price >= 5 * (SELECT st.avg_whole_history
+                                     FROM product_stats st
+                                     WHERE pda.product_id = st.product_id));
+
 DELETE
 FROM product_stats
 WHERE 1 = 1
