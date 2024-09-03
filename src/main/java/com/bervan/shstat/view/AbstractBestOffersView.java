e712d740b474939e11e2bb08d4c1a72fbc79571b
berluk12@gmail.com <berluk12@gmail.com>
2024-09-03 18:37:56 +0200

    ProductsView

--- src/main/java/com/bervan/shstat/view/AbstractBestOffersView.java
+++ src/main/java/com/bervan/shstat/view/AbstractBestOffersView.java
@@ -4,17 +4,21 @@ import com.bervan.common.AbstractPageView;
 import com.bervan.core.model.BervanLogger;
 import com.bervan.shstat.SearchService;
 import com.bervan.shstat.response.ApiResponse;
+import com.bervan.shstat.response.ProductDTO;
+import com.bervan.shstat.response.SearchApiResponse;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import io.micrometer.common.util.StringUtils;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Set;
 
 public abstract class AbstractBestOffersView extends AbstractPageView {
@@ -35,28 +39,40 @@ public abstract class AbstractBestOffersView extends AbstractPageView {
         ComboBox<String> shopDropdown = new ComboBox<>("Shop:");
         shopDropdown.setItems(Arrays.asList("Media Expert", "RTV Euro AGD", "Morele"));
 
-        IntegerField discountMin = new IntegerField("Discount Min:");
-        IntegerField discountMax = new IntegerField("Discount Max:");
+        NumberField discountMin = new NumberField("Discount Min:");
+        discountMin.setValue(1.0);
+        NumberField discountMax = new NumberField("Discount Max:");
+        discountMax.setValue(100.0);
         IntegerField months = new IntegerField("Number of months:");
+        months.setValue(12);
         IntegerField prevPriceMin = new IntegerField("Previous Price Min:");
+        prevPriceMin.setValue(10);
         IntegerField prevPriceMax = new IntegerField("Previous Price Max:");
+        prevPriceMax.setValue(10000);
 
         TextField name = new TextField("Product Name:");
 
         Button searchButton = new Button("Search");
         searchButton.addClickListener(buttonClickEvent -> {
-            findDiscountsComparedToAVGOnPricesInLastXMonths(Pageable.unpaged(),
-                    discountMin.getValue().toString(), discountMax.getValue().toString(), months.getValue(),
-                    prevPriceMin.getValue(), prevPriceMax.getValue(), name.getValue(), categoryDropdown.getValue(),
-                    shopDropdown.getValue());
+            ResponseEntity<ApiResponse> discountsComparedToAVGOnPricesInLastXMonths = findDiscountsComparedToAVGOnPricesInLastXMonths(Pageable.ofSize(50),
+                    discountMin.getValue(), discountMax.getValue(), months.getValue(), prevPriceMin.getValue(), prevPriceMax.getValue(), name.getValue(), categoryDropdown.getValue(), shopDropdown.getValue());
+
+            SearchApiResponse body = (SearchApiResponse) discountsComparedToAVGOnPricesInLastXMonths.getBody();
+            for (Object item : body.getItems()) {
+                ProductDTO productDTO = ((ProductDTO) item);
+
+                VerticalLayout productLayout = new VerticalLayout();
+                Image image = new Image(productDTO.getImgSrc(), "No image :(");
+                productLayout.add(image, new Text(productDTO.getName()), new Text("Price:" + productDTO.getPrices().get(0).getPrice()));
+            }
         });
 
-        add(shopDropdown, categoryDropdown, discountMin, discountMax, prevPriceMin, prevPriceMax, name, searchButton);
+        add(shopDropdown, categoryDropdown, discountMin, discountMax, months, prevPriceMin, prevPriceMax, name, searchButton);
     }
 
     private ResponseEntity<ApiResponse> findDiscountsComparedToAVGOnPricesInLastXMonths(Pageable pageable,
-                                                                                        String discountMin,
-                                                                                        String discountMax,
+                                                                                        Double discountMin,
+                                                                                        Double discountMax,
                                                                                         Integer months,
                                                                                         Integer prevPriceMin,
                                                                                         Integer prevPriceMax,
@@ -67,14 +83,8 @@ public abstract class AbstractBestOffersView extends AbstractPageView {
             category = null;
         }
 
-        if (discountMin.endsWith("%") && discountMax.endsWith("%")) {
-            String numberMin = discountMin.split("%")[0];
-            String numberMax = discountMax.split("%")[0];
-            return ResponseEntity.ok(discountsViewService.findDiscountsComparedToAVGOnPricesInLastXMonths(pageable, Double.parseDouble(numberMin),
-                    Double.parseDouble(numberMax), months, category, shop, name, prevPriceMin, prevPriceMax));
-        } else {
-            return new ResponseEntity<>(new ApiResponse(Collections.singletonList("The discount should be a percentage.\nThe months must be positive."))
-                    , HttpStatus.BAD_REQUEST);
-        }
+        return ResponseEntity.ok(discountsViewService.findDiscountsComparedToAVGOnPricesInLastXMonths(pageable, discountMin,
+                discountMax, months, category, shop, name, prevPriceMin, prevPriceMax));
+
     }
 }
---
+++ src/main/java/com/bervan/shstat/view/AbstractProductView.java
@@ -0,0 +1,69 @@
+package com.bervan.shstat.view;
+
+import com.bervan.common.AbstractPageView;
+import com.bervan.core.model.BervanLogger;
+import com.bervan.shstat.SearchService;
+import com.bervan.shstat.response.PriceDTO;
+import com.bervan.shstat.response.ProductDTO;
+import com.bervan.shstat.response.SearchApiResponse;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public abstract class AbstractProductView extends AbstractPageView {
+    public static final String ROUTE_NAME = "/shopping/products";
+    private final ProductViewService productViewService;
+    private final SearchService searchService;
+    private final BervanLogger log;
+
+    public AbstractProductView(ProductViewService productViewService, SearchService searchService, BervanLogger log) {
+        super();
+        this.productViewService = productViewService;
+        this.searchService = searchService;
+        this.log = log;
+        Set<String> categories = this.searchService.findCategories();
+        ComboBox<String> categoryDropdown = new ComboBox<>("Category:");
+        categoryDropdown.setItems(categories);
+
+        ComboBox<String> shopDropdown = new ComboBox<>("Shop:");
+        shopDropdown.setItems(Arrays.asList("Media Expert", "RTV Euro AGD", "Morele"));
+
+        TextField name = new TextField("Product Name: NOT WORKING YET");
+
+        Button searchButton = new Button("Search");
+        VerticalLayout productsLayout = new VerticalLayout();
+
+        searchButton.addClickListener(buttonClickEvent -> {
+            SearchApiResponse products = getProductList(categoryDropdown.getValue(), shopDropdown.getValue(), Pageable.ofSize(500));
+            productsLayout.removeAll();
+            for (Object item : products.getItems()) {
+                ProductDTO productDTO = ((ProductDTO) item);
+
+                VerticalLayout productLayout = new VerticalLayout();
+                Image image = new Image(productDTO.getImgSrc() == null ? "" : productDTO.getImgSrc(), "No image :(");
+                List<PriceDTO> prices = productDTO.getPrices();
+                Text priceText = new Text("No price");
+                if (prices != null && prices.size() > 0) {
+                    priceText = new Text("Price:" + prices.get(0).getPrice());
+                }
+                productLayout.add(image, new Text(productDTO.getName()), priceText);
+                productsLayout.add(productLayout);
+            }
+        });
+
+        add(shopDropdown, categoryDropdown, name, searchButton, productsLayout);
+    }
+
+    public SearchApiResponse getProductList(String category, String shop, Pageable pageable) {
+        return productViewService.findProductsByCategory(category, shop, pageable);
+    }
+}
