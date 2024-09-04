7e7e8f1c7d4e6e22ba238205a066acdaa408387d
berluk12@gmail.com <berluk12@gmail.com>
2024-09-04 22:27:17 +0200

    RefreshingMaterializedViews cron scheduled

---
+++ src/main/java/com/bervan/shstat/ShopSchedulerTasks.java
@@ -0,0 +1,39 @@
+package com.bervan.shstat;
+
+import ch.qos.logback.core.testUtil.RandomUtil;
+import com.bervan.core.model.BervanLogger;
+import com.bervan.shstat.favorites.FavoriteController;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShopSchedulerTasks {
+    private final FavoriteController favoriteController;
+    private final ProductController productController;
+    private final BervanLogger log;
+
+    public ShopSchedulerTasks(FavoriteController favoriteController, ProductController productController, BervanLogger log) {
+        this.favoriteController = favoriteController;
+        this.productController = productController;
+        this.log = log;
+    }
+
+    @Scheduled(cron = "0 0 3 * * *")
+    public void refreshView() {
+        try {
+            productController.refreshMaterializedViews();
+        } catch (Exception e) {
+            log.error("RefreshingViews: FAILED!", e);
+        }
+    }
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void refreshFavorites() throws InterruptedException {
+        Thread.sleep(15000 + RandomUtil.getPositiveInt() % 15000);
+        try {
+            favoriteController.refreshMaterializedViews();
+        } catch (Exception e) {
+            log.error("RefreshingViews: FAILED!", e);
+        }
+    }
+}
