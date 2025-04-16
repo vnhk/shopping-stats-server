aa1ff54ec9c0475881072fac3a9a56eeea69cc6d
berluk12@gmail.com <berluk12@gmail.com>
2025-04-16 12:40:52 +0200

    PRSTAT-8614 DB Setup

--- src/main/java/com/bervan/shstat/repository/ProductRepository.java
+++ src/main/java/com/bervan/shstat/repository/ProductRepository.java
@@ -6,7 +6,6 @@ import com.bervan.shstat.entity.ProductBasedOnDateAttributes;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
