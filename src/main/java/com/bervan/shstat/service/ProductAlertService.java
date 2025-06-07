80da20549dff0398f0785916d624e679b08f71c5
berluk12@gmail.com <berluk12@gmail.com>
2025-06-07 12:48:42 +0200

    Notifying simplified, changed to multiple queries

--- src/main/java/com/bervan/shstat/service/ProductAlertService.java
+++ src/main/java/com/bervan/shstat/service/ProductAlertService.java
@@ -139,7 +139,6 @@ public class ProductAlertService extends BaseService<Long, ProductAlert> {
 
             if (match) {
                 matchedProducts.add(product);
-                break;
             }
         }
 
