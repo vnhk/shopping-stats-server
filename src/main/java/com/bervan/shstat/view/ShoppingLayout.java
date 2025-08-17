e306f41a12005a0becf4b2cd9f5512a993b2efba
berluk12@gmail.com <berluk12@gmail.com>
2025-08-17 14:27:16 +0200

    New Navigation menu styling

--- src/main/java/com/bervan/shstat/view/ShoppingLayout.java
+++ src/main/java/com/bervan/shstat/view/ShoppingLayout.java
@@ -1,19 +1,20 @@
 package com.bervan.shstat.view;
 
 import com.bervan.common.MenuNavigationComponent;
+import com.vaadin.flow.component.icon.VaadinIcon;
 
 public class ShoppingLayout extends MenuNavigationComponent {
 
     public ShoppingLayout(String currentRoute) {
         super(currentRoute);
 
-        addButtonIfVisible(menuButtonsRow, AbstractProductsView.ROUTE_NAME, "Search");
-        addButtonIfVisible(menuButtonsRow, AbstractBestOffersView.ROUTE_NAME, "Best Offers");
-        addButtonIfVisible(menuButtonsRow, AbstractProductView.ROUTE_NAME, "Product");
-        addButtonIfVisible(menuButtonsRow, AbstractShopConfigView.ROUTE_NAME, "Shop Config");
-        addButtonIfVisible(menuButtonsRow, AbstractProductConfigView.ROUTE_NAME, "Product Config");
-        addButtonIfVisible(menuButtonsRow, AbstractScrapAuditView.ROUTE_NAME, "Scrap Audit");
-        addButtonIfVisible(menuButtonsRow, AbstractProductAlertView.ROUTE_NAME, "Product Alerts");
+        addButtonIfVisible(menuButtonsRow, AbstractProductsView.ROUTE_NAME, "Search", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractBestOffersView.ROUTE_NAME, "Best Offers", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractProductView.ROUTE_NAME, "Product", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractShopConfigView.ROUTE_NAME, "Shop Config", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractProductConfigView.ROUTE_NAME, "Product Config", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractScrapAuditView.ROUTE_NAME, "Scrap Audit", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractProductAlertView.ROUTE_NAME, "Product Alerts", VaadinIcon.HOME.create());
         add(menuButtonsRow);
     }
 }
