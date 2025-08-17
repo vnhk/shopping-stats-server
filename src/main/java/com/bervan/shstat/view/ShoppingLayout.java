fb09e23660047ad8ad237b6c191e7ef36482c313
berluk12@gmail.com <berluk12@gmail.com>
2025-08-17 14:35:05 +0200

    New Navigation menu icons

--- src/main/java/com/bervan/shstat/view/ShoppingLayout.java
+++ src/main/java/com/bervan/shstat/view/ShoppingLayout.java
@@ -8,13 +8,13 @@ public class ShoppingLayout extends MenuNavigationComponent {
     public ShoppingLayout(String currentRoute) {
         super(currentRoute);
 
-        addButtonIfVisible(menuButtonsRow, AbstractProductsView.ROUTE_NAME, "Search", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractBestOffersView.ROUTE_NAME, "Best Offers", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractProductView.ROUTE_NAME, "Product", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractShopConfigView.ROUTE_NAME, "Shop Config", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractProductConfigView.ROUTE_NAME, "Product Config", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractScrapAuditView.ROUTE_NAME, "Scrap Audit", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractProductAlertView.ROUTE_NAME, "Product Alerts", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractProductsView.ROUTE_NAME, "Search", VaadinIcon.SEARCH.create());
+        addButtonIfVisible(menuButtonsRow, AbstractBestOffersView.ROUTE_NAME, "Best Offers", VaadinIcon.THUMBS_UP.create());
+        addButtonIfVisible(menuButtonsRow, AbstractProductView.ROUTE_NAME, "Product", VaadinIcon.PACKAGE.create());
+        addButtonIfVisible(menuButtonsRow, AbstractShopConfigView.ROUTE_NAME, "Shop Config", VaadinIcon.COG.create());
+        addButtonIfVisible(menuButtonsRow, AbstractProductConfigView.ROUTE_NAME, "Product Config", VaadinIcon.TOOLS.create());
+        addButtonIfVisible(menuButtonsRow, AbstractScrapAuditView.ROUTE_NAME, "Scrap Audit", VaadinIcon.CLIPBOARD_TEXT.create());
+        addButtonIfVisible(menuButtonsRow, AbstractProductAlertView.ROUTE_NAME, "Product Alerts", VaadinIcon.BELL.create());
         add(menuButtonsRow);
     }
 }
